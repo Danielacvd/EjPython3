f23fae05a9eb67849b286e8443b4ffdc1c20b751
Daniel <dacevedosp@gmail.com>
2020-09-30 22:23:10 -0300

    Ej8 Done2

--- Ej8.py
+++ Ej8.py
@@ -1,6 +1,19 @@
 # Ejercicio 8: Obtener el primer y último de una lista.
-#Try1
+#Try2
 lenguajes = ['Python', 'C#', 'PHP', 'Java', 'JavaScript']
 
-print(lenguajes[0])
-print(lenguajes[-1])
+#Try1
+# print(lenguajes[0])
+# print(lenguajes[-1])
+
+def primero_ultimo(lista):
+    res = ""
+    if lista[0]:
+        res += f"{lista[0]}"    
+    if lista[-1]:
+      res += f" {lista[-1]}"
+    return res        
+
+print(primero_ultimo(lenguajes))
+
+
