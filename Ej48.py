923bb0cc1cd1671db7d30ebe4c2bce7476c04838
Daniel <dacevedosp@gmail.com>
2020-10-01 20:19:49 -0300

    Ej48 Done1

---
+++ Ej48.py
@@ -0,0 +1,9 @@
+# Ejercicio 48: Convertir una cadena de caracteres a entero y real.
+#Try1
+# int(), float()
+
+cadena = '3.1415'
+cadena_entero = int(cadena[0])
+cadena_real = float(cadena)
+print(cadena_entero)
+print(cadena_real)
