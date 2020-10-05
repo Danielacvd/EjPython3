1570ead1732638a1de16a9e295f75b185c1d236b
Daniel <dacevedosp@gmail.com>
2020-10-04 21:48:07 -0300

    Ej39 Done2

--- Ej39.py
+++ Ej39.py
@@ -1,11 +1,19 @@
 # Ejercicio 39: Calcular el valor futuro para una cantidad, interés, y número de años específicos.
-#Try1
+#Try2
 
 # VF = VP(1+i)^n
+#Try1
+# valor_presente = 10000
+# interes = 3.5
+# periodos = 7
+# Vf = valor_presente * (1 + interes/100)**7
+# print(Vf)
+
+def valor_futuro(vp, i, n):
+    return f"{(vp * (1 + i/100)**n)}"
 
 valor_presente = 10000
 interes = 3.5
 periodos = 7
 
-Vf = valor_presente * (1 + interes/100)**7
-print(Vf)
+print(valor_futuro(valor_presente, interes, periodos))
