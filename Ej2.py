d59639380f2695bd13b8f5c5ff112af03b91fd0e
Daniel <dacevedosp@gmail.com>
2020-10-20 22:44:07 -0300

    Ej2 Try2

--- Ej2.py
+++ Ej2.py
@@ -1,7 +1,13 @@
 # Ejercicio 2: Exponer el uso básico de la función print.
-#Try 1
+#Try 2
 saludo = "Hola"
 saludo2 = f"{saludo} 2"
 print(saludo)
 print(f"Con Interpolacion: {saludo2}")
 print(""+saludo)
+
+
+#2
+nombre = 'Daniel'
+edad = 15
+print("Hola ", nombre, " tienes:",edad, "annos")
