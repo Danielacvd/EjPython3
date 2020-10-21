b737083513acbc5693abae3152e02d1205960bdd
Daniel <dacevedosp@gmail.com>
2020-10-20 22:51:15 -0300

    Ej21 Try2

--- Ej21.py
+++ Ej21.py
@@ -1,17 +1,22 @@
 # Ejercicio 21: Generar los n primeros números pares positivos.
+#Try2
+
 #Try1
-
-
 def pares(n):
-    i = 0
+    i = 1
     while i <= n:
         if i % 2 == 0:
             print(i)
         i += 1
     else:
-        print("Fin ciclo...")
-
-
+        print("Fin ciclo while ...")
 
 numero = int(input("Ingresa un numero: "))
 pares(numero)
+#Try2
+#con for
+for i in range(1,numero+1):
+    if i % 2 == 0:
+        print(i)
+else:
+    print("Fin ciclo for")        
