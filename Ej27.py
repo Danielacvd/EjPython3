4d0d088cab5b2ac0e5906940b35121487c9a7e7f
Daniel <dacevedosp@gmail.com>
2020-10-20 22:59:51 -0300

    Ej27 Try2

--- Ej27.py
+++ Ej27.py
@@ -14,3 +14,21 @@ print(lista)
 for i in lista:
     if not i % 2 == 0:
         print(i) 
+
+
+#Try2
+print("***Solucion2***")
+def impares(n):
+    lista2 = []
+    i = 0
+    while i < n:
+        lista2.append(randint(1, 10000))
+        i += 1
+    resultado2 = []    
+    for i in lista2:
+        if not i % 2 == 0:
+            resultado2.append(i)
+
+    print(f"De la lista {lista2}\nEstos son los numero impares: {resultado2}")   
+
+impares(n)     
