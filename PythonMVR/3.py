dbdb8e49dc90a4d51caaace1960c328e1f271045
Daniel <dacevedosp@gmail.com>
2020-11-10 19:20:53 -0300

    Ej3 Try1 Done

---
+++ PythonMVR/3.py
@@ -0,0 +1,11 @@
+"""
+Pedir 2 numeros al usuario y hacer todas las operaciones basicas de una calculadora con los 2 numeros.
+"""
+#Try1
+numero_1 = input("Ingresa el primero numero: ")
+numero_2 = input("Ingresa el segundo numero: ")
+
+print(f"La suma es: {int(numero_1) + int(numero_2)}")
+print(f"La resta es: {int(numero_1) - int(numero_2)}")
+print(f"La multiplicacion es: {int(numero_1) * int(numero_2)}")
+print(f"La diviion es: {int(numero_1) / float(numero_2)}")
