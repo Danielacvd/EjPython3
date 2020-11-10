c828e3b7843cbe520a4cd6ebf43f4a3513034d10
Daniel <dacevedosp@gmail.com>
2020-11-10 19:15:55 -0300

    Ej2 Try1 Done

---
+++ PythonMVR/2.py
@@ -0,0 +1,11 @@
+"""
+Mostrar por pantalla todos los numeros pares del 1 al 100
+"""
+#Try1
+i = 0
+while i <= 100:
+    if i % 2 == 0:
+        print(i)
+    i += 1
+else:
+    print("Estos son los primeros 100 numeros pares")         
