58f5feb0c709860b1fc3b91766d6dc6e60b8524c
Daniel <dacevedosp@gmail.com>
2020-11-10 19:23:10 -0300

    Ej4 Try1 Done

---
+++ PythonMVR/4.py
@@ -0,0 +1,11 @@
+"""
+Imprimir por pantalla los cuadrados de los primeros 60 numeros naturales.
+"""
+
+#Try1
+i = 0
+while i <= 60:
+    print(i**2)
+    i += 1
+else:
+    print("Cuadrado de los primeros 60 numeros")   
