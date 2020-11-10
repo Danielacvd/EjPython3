1f8124c7e69074433d258909735c370db8c40df1
Daniel <dacevedosp@gmail.com>
2020-11-10 19:13:23 -0300

    Ej1 Try1 Done

---
+++ PythonMVR/1.py
@@ -0,0 +1,9 @@
+"""
+#1- Crear 2 variables, pais, continente, imprimir sus valores por pantalla, muestra el tipo de dato de cada variable
+"""
+
+#Try1
+pais = "Chile"
+continente = "LA"
+print(f"Pais: {pais}, Contienente: {continente}")
+print(f"Tipo de dato de variable pais: {type(pais)}\nTipo de dato variable contienente: {type(continente)}")
