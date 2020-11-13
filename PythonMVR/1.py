fd1f4f7186389ff555fa8f5bc30413dfe0d6a954
Daniel <dacevedosp@gmail.com>
2020-11-12 23:01:02 -0300

    Ej1 Try2 Done

--- PythonMVR/1.py
+++ PythonMVR/1.py
@@ -7,3 +7,10 @@ pais = "Chile"
 continente = "LA"
 print(f"Pais: {pais}, Contienente: {continente}")
 print(f"Tipo de dato de variable pais: {type(pais)}\nTipo de dato variable contienente: {type(continente)}")
+
+#Try2
+pais = lambda pais: f"Pais: {pais}, tipo de dato: {type(pais)}"
+continente = lambda continente: f"Contienen: {continente}, tipo de dato: {type(continente)}"
+
+print(pais('Chile'))
+print(continente('LA'))
