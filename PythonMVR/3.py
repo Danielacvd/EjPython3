2810210bd4727a132a00b363bbd4560876851166
Daniel <dacevedosp@gmail.com>
2020-11-12 23:26:32 -0300

    Ej3 Try2 Done

--- PythonMVR/3.py
+++ PythonMVR/3.py
@@ -9,3 +9,13 @@ print(f"La suma es: {int(numero_1) + int(numero_2)}")
 print(f"La resta es: {int(numero_1) - int(numero_2)}")
 print(f"La multiplicacion es: {int(numero_1) * int(numero_2)}")
 print(f"La diviion es: {int(numero_1) / float(numero_2)}")
+
+#Try2
+suma = lambda n1, n2: f"La suma es: {int(numero_1)+int(numero_2)}"
+print(suma(numero_1, numero_2))
+resta = lambda n1, n2: f"La resta es: {int(numero_1)-int(numero_2)}"
+print(resta(numero_1, numero_2))
+multiplicacion = lambda n1, n2: f"La multiplicacion es: {int(numero_1)*int(numero_2)}"
+print(multiplicacion(numero_1, numero_2))
+division = lambda n1, n2: f"La division es: {int(numero_1)/int(numero_2)}"
+print(division(numero_1, numero_2))
